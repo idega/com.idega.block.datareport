ea7a2d282f4e2143d00853b9eb0c03671753d5c2
Sigtryggur Simonarson <sigtryggur@idega.com>
2007-07-11 08:55:04 +0000

    New handlers for QueryResultViewerLink

---
+++ src/java/com/idega/block/datareport/business/QueryHandler.java
@@ -0,0 +1,50 @@
+package com.idega.block.datareport.business;
+
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ejb.FinderException;
+
+import com.idega.block.dataquery.business.QueryService;
+import com.idega.block.dataquery.data.QueryRepresentation;
+import com.idega.business.IBOLookup;
+import com.idega.core.builder.presentation.ICPropertyHandler;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.ui.DropdownMenu;
+
+public class QueryHandler implements ICPropertyHandler {
+
+	public List getDefaultHandlerTypes() {
+		return null;
+	}
+
+	public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+		DropdownMenu menu = new DropdownMenu(name);
+		try {
+			QueryService business = (QueryService) IBOLookup.getServiceInstance(iwc, QueryService.class);
+			Collection queries = business.getQueries(iwc);
+			if (queries != null) {
+				Iterator iter = queries.iterator();
+				while (iter.hasNext()) {
+					QueryRepresentation element = (QueryRepresentation) iter.next();
+					menu.addMenuElement(element.getPrimaryKey().toString(), (String)element.getColumnValue(QueryRepresentation.NAME_KEY));
+					menu.setSelectedElement(stringValue);
+				}
+			}
+		}
+		catch (RemoteException re) {
+			re.printStackTrace();
+		}
+		catch (FinderException re) {
+			re.printStackTrace();
+		}
+
+		return menu;
+	}
+
+	public void onUpdate(String[] values, IWContext iwc) {
+	}
+}
---
+++ src/java/com/idega/block/datareport/business/QueryOutputFormatHandler.java
@@ -0,0 +1,32 @@
+package com.idega.block.datareport.business;
+
+import java.util.List;
+
+import com.idega.block.datareport.presentation.QueryResultViewer;
+import com.idega.core.builder.presentation.ICPropertyHandler;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.ui.DropdownMenu;
+
+public class QueryOutputFormatHandler implements ICPropertyHandler {
+
+	public static final String ENTITY_IW_BUNDLE_IDENTIFIER = "com.idega.block.entity";
+
+	public List getDefaultHandlerTypes() {
+		return null;
+	}
+
+	public PresentationObject getHandlerObject(String name, String stringValue, IWContext iwc) {
+		IWResourceBundle iwrb = iwc.getIWMainApplication().getBundle(ENTITY_IW_BUNDLE_IDENTIFIER).getResourceBundle(iwc.getCurrentLocale());
+		DropdownMenu menu = new DropdownMenu(name);
+		menu.addMenuElement(QueryResultViewer.EXCEL_KEY, iwrb.getLocalizedString(QueryResultViewer.EXCEL_KEY, QueryResultViewer.EXCEL_KEY));
+		menu.addMenuElement(QueryResultViewer.PDF_KEY, iwrb.getLocalizedString(QueryResultViewer.PDF_KEY, QueryResultViewer.PDF_KEY));
+		menu.addMenuElement(QueryResultViewer.HTML_KEY, iwrb.getLocalizedString(QueryResultViewer.HTML_KEY, QueryResultViewer.HTML_KEY));
+		menu.setSelectedElement(stringValue);
+		return menu;
+	}
+
+	public void onUpdate(String[] arg0, IWContext arg1) {
+	}
+}
