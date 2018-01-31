daf5bf10eade093777b63fd9ce36df5fc016c3b5
amzinas <zygimantas@idega.com>
2018-01-31 13:14:57 +0200

    Select by default all the clubs in work reports.

--- resources/javascript/DataReportGenerator.js
+++ resources/javascript/DataReportGenerator.js
@@ -11,5 +11,27 @@ DataReportGenerator.submit = function(formId, paramState, valueState) {
 	var input = jQuery(form[paramState]);
 	input.val(valueState);
 	
+	//Select all the clubs, if not selected at all
+	DataReportGenerator.selectAllInSelectionBoxIfNoneSelected(jQuery(form['dr_clubs']));
+	DataReportGenerator.selectAllInSelectionBoxIfNoneSelected(jQuery(form["dr_umficlubs"]));
+	
 	form.submit();
 }
+
+DataReportGenerator.selectAllInSelectionBoxIfNoneSelected = function(input) {
+	if (input != null) {
+		var noElementsSelected = true;
+		var options = input[0];
+		for( i = 0; i < options.length; i++ ) {
+		    if (options[i].selected) {
+		      noElementsSelected = false;
+		    }
+		}
+		if (noElementsSelected){
+		    for( i = 0; i < options.length; i++ ) {
+		    	options[i].selected = true;
+		    }
+		}
+	}
+}
+
